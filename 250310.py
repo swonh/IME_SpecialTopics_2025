2f54ad29600b4ce3e2e1b40804fdd0455ac6b83c
Sungwon Hong <143929490+swonh@users.noreply.github.com>
2025-03-09 15:22:07 +0900

    Commit.

---
+++ 250310.py
@@ -0,0 +1,10 @@
+def find_max(A):
+    max_val = 0
+    for i in range(len(A)):
+        if max_val < A[i]:
+            max_val = A[i]
+    return max_val
+
+A = [1, 20, 30, 3]
+print(find_max(A))
+
